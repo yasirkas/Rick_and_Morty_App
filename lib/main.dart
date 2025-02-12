be4fcc73933ec673b38471f0eca80102e4175efe
Yasir Kaş <102860490+yasirkas@users.noreply.github.com>
2025-02-12 10:38:26 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:rick_and_morty_app/home_page_.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Rick and Morty App',
+      theme: ThemeData.light(),
+      home: HomePage(),
+    );
+  }
+}
+
