b4bdd9dfd7b971ab0fe6112ea80b4c008f8ef307
Yasir Kaş <102860490+yasirkas@users.noreply.github.com>
2025-02-13 12:56:44 +0300

    character detail page created

---
+++ lib/pages/character/character_details.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:rick_and_morty_app/models/character_model.dart';
+
+class CharacterDetails extends StatelessWidget {
+  const CharacterDetails({super.key, required this.character});
+  final CharacterModel character;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(character.name ?? ''),
+        backgroundColor: Colors.blueGrey,
+        foregroundColor: Colors.white,
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Image.network(
+                character.image ?? '',
+                width: 200,
+                height: 200,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Species: ${character.species}',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Gender: ${character.gender}',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Location: ${character.location?.name}',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Origin: ${character.origin?.name}',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Episodes: ${character.episode?.length}',
+              style: TextStyle(fontSize: 18),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/character/character_page.dart
+++ lib/pages/character/character_page.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:rick_and_morty_app/models/character_model.dart';
+import 'package:rick_and_morty_app/pages/character/character_details.dart';
 import 'package:rick_and_morty_app/statics/static_texts.dart';
 
 class CharacterPage extends StatefulWidget {
@@ -81,56 +82,67 @@ class _CharacterCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Card(
-        color: Colors.blueGrey,
-        child: Row(
-          children: [
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.4,
-              child: Image.network(
-                _model?.image ?? '',
-                fit: BoxFit.cover,
-              ),
+    return InkWell(
+      onTap: () {
+        if (_model != null) {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => CharacterDetails(character: _model),
             ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.only(left: 4),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      _model?.name ?? '',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                    ),
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.circle,
-                          size: 12,
-                          color: (_model?.status == 'Alive')
-                              ? Colors.green
-                              : (_model?.status == 'Dead')
-                                  ? Colors.red
-                                  : Colors.grey,
-                        ),
-                        Text(
-                            '${_model?.status ?? ''} - ${_model?.species ?? ''}'),
-                      ],
-                    ),
-                    SizedBox(height: 4),
-                    Text(
-                        '${StaticTexts.cardFirstSubtitle}:\n${_model?.location?.name ?? ''}'),
-                    SizedBox(height: 12),
-                    Text(
-                        '${StaticTexts.cardSecondSubtitle}:\n${_model?.origin?.name ?? ''}'),
-                  ],
+          );
+        }
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Card(
+          color: Colors.blueGrey,
+          child: Row(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.4,
+                child: Image.network(
+                  _model?.image ?? '',
+                  fit: BoxFit.cover,
                 ),
               ),
-            ),
-          ],
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 4),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        _model?.name ?? '',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16),
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.circle,
+                            size: 12,
+                            color: (_model?.status == 'Alive')
+                                ? Colors.green
+                                : (_model?.status == 'Dead')
+                                    ? Colors.red
+                                    : Colors.grey,
+                          ),
+                          Text(
+                              '${_model?.status ?? ''} - ${_model?.species ?? ''}'),
+                        ],
+                      ),
+                      SizedBox(height: 4),
+                      Text(
+                          '${StaticTexts.cardFirstSubtitle}:\n${_model?.location?.name ?? ''}'),
+                      SizedBox(height: 12),
+                      Text(
+                          '${StaticTexts.cardSecondSubtitle}:\n${_model?.origin?.name ?? ''}'),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
